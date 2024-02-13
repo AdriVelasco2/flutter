bfb57d35597fdfcd84e8dd522f7dedba4ff9b0a9
luishurleUp <luis.hurle.upintelligence@gmail.com>
2024-02-13 08:53:45 +0100

    Pruebas

---
+++ lib/index.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+    title:'App de Adri',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      
+     )
+    );
+  }
+
+}
